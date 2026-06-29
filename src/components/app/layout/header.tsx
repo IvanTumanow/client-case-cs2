21e5a78055ce4d78c15f76ab8871cedc5c80a4cf
Ivan <welka2018@mail.ru>
2026-06-29 15:42:21 +0300

    updated header to shadcn component; add spinner of ui component; add base structure balance store, store is uncorrected

--- src/components/app/layout/header.tsx
+++ src/components/app/layout/header.tsx
@@ -1,11 +1,12 @@
 import {ROUTES_CONFIG} from "@/config/routes.config.tsx";
-import {Link} from "react-router";
 import ThemeSwitcher from "@/components/app/ui/ThemeSwitcher.tsx";
 import {api} from "@/lib/axios/axios.ts";
 import {SERVER_CONFIG} from "@/config/server.config.ts";
 import type {Route} from "@/shared/types/route.types.ts";
 import {useEffect, useState} from "react";
 import {Card, CardContent} from "@/components/ui/card.tsx";
+import Spinner from "@/components/app/ui/Spinner.tsx";
+import {NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger} from "@/components/ui/navigation-menu";
 
 export default function Header() {
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
@@ -28,40 +29,71 @@ export default function Header() {
 
     const routes: Route[] =
         [
-            ROUTES_CONFIG.ROUTES.HOME,
-            isAuthenticated ? ROUTES_CONFIG.ROUTES.PROFILE : ROUTES_CONFIG.ROUTES.AUTH
+            ROUTES_CONFIG.ROUTES.HOME
         ];
 
+    if (!isAuthenticated) routes.push(ROUTES_CONFIG.ROUTES.AUTH);
+
     return (
-        <header className={'sticky top-0 w-full py-2'}>
-            <nav className={'w-full flex flex-row items-center justify-center'}>
-                <ul className={'w-fit flex flex-row justify-center items-center gap-5 bg-background border py-2.5 px-20 rounded-2xl'}>
+        <header className={'flex flex-row justify-center items-center sticky top-0 w-full py-2'}>
+            <NavigationMenu>
+                <NavigationMenuList  className={'w-fit flex flex-row justify-center items-center gap-5 bg-background border px-20 rounded-2xl'}>
                     {
                         !loading &&
                         <>
                             {
                                 routes.map((item, index) => (
-                                    <li
+                                    <NavigationMenuItem
                                         key={`header-element-${index}_${item.title}`}
-                                        className={'inline-flex'}
                                     >
-                                        <Link to={item.url} title={item.title}>
+                                        <NavigationMenuLink href={item.url} title={item.title}>
                                             {item?.titleElement ? item.titleElement : item.title}
-                                        </Link>
-                                    </li>
+                                        </NavigationMenuLink>
+                                    </NavigationMenuItem>
                                 ))
+                            }
+
+                            {
+                                isAuthenticated &&
+                                <>
+                                    <NavigationMenuItem>
+                                        <NavigationMenuTrigger>
+                                            {ROUTES_CONFIG.ROUTES.PROFILE.titleElement ? ROUTES_CONFIG.ROUTES.PROFILE.titleElement : ROUTES_CONFIG.ROUTES.PROFILE.title}
+                                        </NavigationMenuTrigger>
+                                        <NavigationMenuContent className={'min-w-40'}>
+                                            <NavigationMenuLink
+                                                href={ROUTES_CONFIG.ROUTES.PROFILE.url}
+                                                title={ROUTES_CONFIG.ROUTES.PROFILE.title}
+                                                className={'w-full'}
+                                            >
+                                                {ROUTES_CONFIG.ROUTES.PROFILE.title}
+                                            </NavigationMenuLink>
+
+                                            <NavigationMenuLink className={'w-full cursor-pointer'} asChild href={'/'}>
+                                                <div className="flex flex-col text-sm justify-start items-start">
+                                                    <p className="leading-0 font-medium">Баланс</p>
+                                                    <p className="line-clamp-2 text-muted-foreground">Пополнить баланс</p>
+                                                </div>
+                                            </NavigationMenuLink>
+                                        </NavigationMenuContent>
+                                    </NavigationMenuItem>
+                                </>
                             }
                         </>
                     }
 
                     {
                         loading &&
-                        <li>Загрузка...</li>
+                        <>
+                            <NavigationMenuItem>
+                                <NavigationMenuLink>
+                                    <Spinner/>
+                                </NavigationMenuLink>
+                            </NavigationMenuItem>
+                        </>
                     }
-                </ul>
-            </nav>
-
-
+                </NavigationMenuList>
+            </NavigationMenu>
 
             <Card className={'fixed right-0 top-30 w-fit p-1 flex flex-col justify-center items-center rounded-l-full'}>
                 <CardContent className={'p-1'}>
---
+++ src/components/app/ui/Spinner.tsx
@@ -0,0 +1,7 @@
+import {Loader as LoaderCircle} from "lucide-react";
+
+export default function Spinner () {
+    return (
+        <LoaderCircle className={'animate-spin duration-3000'}/>
+    )
+}
